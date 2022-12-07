1e6d1f7dba88baf48fff8d0aa1080949a93ab92a
Koen Vanderperren <koen.vanderperren@uniweb.eu>
2022-12-07 14:11:26 +0100

    Complete day 3

---
+++ day03.php
@@ -0,0 +1,71 @@
+<?php
+class solution extends abstractSolution
+{
+    public function run($partNr)
+    {
+        if ($partNr == 2) {
+            $this->part2();
+        } else {
+            $this->part1();
+        }
+    }
+
+    private function part1()
+    {
+        $sum = 0;
+        foreach ($this->data as $line) {
+            $size = strlen($line) / 2;
+            $compartments = str_split($line, $size);
+            $charsCompartmentA = str_split($compartments[0]);
+            $commonChar = '';
+            for ($i = 0; $i < $size; $i++) {
+                if (in_array($compartments[1][$i], $charsCompartmentA)) {
+                    $commonChar = $compartments[1][$i];
+                    break;
+                }
+            }
+
+            $points = $this->getPoints($commonChar);
+            $this->output($compartments[0] . ' ' . $compartments[1] . ' -> ' . $commonChar . ' (' . $points . ')');
+            $sum += $points;
+        }
+
+        $this->output("\nSum: " . $sum, 'blue');
+    }
+
+    private function part2()
+    {
+        $sum = 0;
+        $max = count($this->data);
+        for ($i = 0; $i < $max; $i += 3) {
+            $commonChar = '';
+            $charsElve1 = str_split($this->data[$i]);
+            $charsElve2 = str_split($this->data[$i + 1]);
+
+            $backpackSize = strlen($this->data[$i + 2]);
+            for ($j = 0; $j < $backpackSize; $j++) {
+                $char = $this->data[$i + 2][$j];
+                if (in_array($char, $charsElve1) && in_array($char, $charsElve2)) {
+                    $commonChar = $char;
+                    break;
+                }
+            }
+
+            $points = $this->getPoints($commonChar);
+            $this->output($this->data[$i] . ' ' . $this->data[$i + 1] . ' ' . $this->data[$i + 2] . ' -> ' . $commonChar . ' (' . $points . ')');
+            $sum += $points;
+        }
+
+        $this->output("\nSum: " . $sum, 'blue');
+    }
+
+    private function getPoints($char)
+    {
+        $ord = ord($char);
+        if ($ord < 97) {
+            return $ord - 38;
+        } else {
+            return $ord - 96;
+        }
+    }
+}
